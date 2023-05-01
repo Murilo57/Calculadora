8c9d8d54ada360ccfb40a662276ceae4b105bde1
Murilo Ferreira <115593380+Murilo57@users.noreply.github.com>
2023-05-01 11:17:29 -0300

    Commit1

---
+++ PROJECT/ProjCalculadora.dpr
@@ -0,0 +1,13 @@
+program ProjCalculadora;
+
+uses
+  Forms,
+  UPrincila in '..\FORMS\UPrincila.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
